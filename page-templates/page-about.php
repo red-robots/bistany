87e302ca9795264e37ca40e8ee6d8d048dd6f450
Lisa <lisaqdebona@gmail.com>
2023-09-20 14:01:51 -0400

    styled about page

--- page-templates/page-about.php
+++ page-templates/page-about.php
@@ -17,21 +17,12 @@ if( $background_image_url != '') {
 
 <div class="background-image" style="background-image: url(<?php echo $background_image_url; ?>);"></div>
 
-<div class="grad"></div>
 <div class="primary <?php echo $theClass; ?> js-scroll-div">
-
 	<?php while(have_posts()) : the_post(); ?>
-		<header class="title">
-			<h1><?php the_title(); ?></h1>
-		</header>
-
-		<section class="work-content entry-content">
-			<?php the_content(); ?>
-		</section>
-
+  <div class="flexwrap">
+		<?php the_content(); ?>
+  </div>
 	<?php endwhile; ?>
-
-
 </div>
 <!-- primary -->
 <?php get_footer(); ?>
